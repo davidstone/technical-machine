e021d27809e9ef6c775bdcd35b98a8e6900c69d5
David Stone <david@doublewise.net>
2011-07-19 13:08:06 -0600

    Fixed typo in include guard.

--- pokemon_lab/file.h
+++ pokemon_lab/file.h
@@ -10,7 +10,7 @@
 // You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef POKEMON_LAB_FILE_H_
-#define FILE_H_
+#define POKEMON_LAB_FILE_H_
 
 #include <fstream>
 #include <string>
