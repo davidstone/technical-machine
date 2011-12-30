37d927f5138c78598ffb7908f618a6795dae5abc
David Stone <david@doublewise.net>
2011-12-29 18:03:21 -0700

    Added missing nature files.

---
+++ src/nature.cpp
@@ -0,0 +1,80 @@
+// Stat formulas
+// Copyright (C) 2011 David Stone
+//
+// This file is part of Technical Machine.
+//
+// Technical Machine is free software: you can redistribute it and / or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include "nature.hpp"
+
+#include <map>
+#include <string>
+
+namespace technicalmachine {
+
+Nature::Nature ():
+	name (END_NATURE) {
+}
+
+bool Nature::is_set () const {
+	return name != END_NATURE;
+}
+
+std::string Nature::to_string (Natures name) {
+	std::string const nature_converter [] = {
+		"Adamant", "Bashful", "Bold", "Brave", "Calm", "Careful",
+		"Docile", "Gentle", "Hardy", "Hasty", "Impish", "Jolly",
+		"Lax", "Lonely", "Mild", "Modest", "Naive", "Naughty",
+		"Quiet", "Quirky", "Rash", "Relaxed", "Sassy", "Serious",
+		"Timid", "End_Nature"
+	};
+	return nature_converter [name];
+}
+
+Nature::Natures Nature::from_string (std::string const & str) {
+	static std::map <std::string, Natures> const converter {
+		{ "Adamant", ADAMANT },
+		{ "Bashful", BASHFUL },
+		{ "Bold", BOLD },
+		{ "Brave", BRAVE },
+		{ "Calm", CALM },
+		{ "Careful", CAREFUL },
+		{ "Docile", DOCILE },
+		{ "Gentle", GENTLE },
+		{ "Hardy", HARDY },
+		{ "Hasty", HASTY },
+		{ "Impish", IMPISH },
+		{ "Jolly", JOLLY },
+		{ "Lax", LAX },
+		{ "Lonely", LONELY },
+		{ "Mild", MILD },
+		{ "Modest", MODEST },
+		{ "Naive", NAIVE },
+		{ "Naughty", NAUGHTY },
+		{ "Quiet", QUIET },
+		{ "Quirky", QUIRKY },
+		{ "Rash", RASH },
+		{ "Relaxed", RELAXED },
+		{ "Sassy", SASSY },
+		{ "Serious", SERIOUS },
+		{ "Timid", TIMID }
+	};
+	return converter.find (str)->second;
+}
+
+void Nature::set_name_from_string (std::string const & str) {
+	name = from_string (str);
+}
+
+} // namespace technicalmachine
---
+++ src/nature.hpp
@@ -0,0 +1,65 @@
+// Nature data structure
+// Copyright (C) 2011 David Stone
+//
+// This file is part of Technical Machine.
+//
+// Technical Machine is free software: you can redistribute it and / or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef NATURE_H_
+#define NATURE_H_
+
+#include <string>
+
+namespace technicalmachine {
+
+class Nature {
+	public:
+		enum Natures {
+			ADAMANT,
+			BASHFUL,
+			BOLD,
+			BRAVE,
+			CALM,
+			CAREFUL,
+			DOCILE,
+			GENTLE,
+			HARDY,
+			HASTY,
+			IMPISH,
+			JOLLY,
+			LAX,
+			LONELY,
+			MILD,
+			MODEST,
+			NAIVE,
+			NAUGHTY,
+			QUIET,
+			QUIRKY,
+			RASH,
+			RELAXED,
+			SASSY,
+			SERIOUS,
+			TIMID,
+			END_NATURE
+		};
+		Natures name;
+		Nature ();
+		bool is_set () const;
+		static std::string to_string (Natures name);
+		static Natures from_string (std::string const & str);
+		void set_name_from_string (std::string const & str);
+};
+
+}	// namespace technicalmachine
+#endif	// NATURE_H_
