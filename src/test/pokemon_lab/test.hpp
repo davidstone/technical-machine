ae33715f0a353dc821806b695f51d80946788182
David Stone <david@doublewise.net>
2012-01-08 15:19:58 -0700

    Added base simulator test files.

---
+++ src/test/pokemon_lab/test.hpp
@@ -0,0 +1,30 @@
+// Test PL stuff
+// Copyright (C) 2012 David Stone
+//
+// This file is part of Technical Machine.
+//
+// Technical Machine is free software: you can redistribute it and / or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef TEST_POKEMON_LAB_TEST_HPP_
+#define TEST_POKEMON_LAB_TEST_HPP_
+
+namespace technicalmachine {
+namespace pl {
+
+void test ();
+
+}	// namespace pl
+}	// namespace technicalmachine
+
+#endif	// TEST_POKEMON_LAB_TEST_HPP_
