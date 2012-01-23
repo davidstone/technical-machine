4b2b54b93c691f535050d2c93e0f5ec6a00d8a91
David Stone <david@doublewise.net>
2012-01-22 22:45:20 -0700

    Refactored block.cpp

--- src/block.cpp
+++ src/block.cpp
@@ -1,5 +1,5 @@
 // Block selection and execution of moves
-// Copyright (C) 2011 David Stone
+// Copyright (C) 2012 David Stone
 //
 // This file is part of Technical Machine.
 //
@@ -25,38 +25,42 @@
 #include "weather.hpp"
 
 namespace technicalmachine {
-
 namespace {
-static bool imprison (Move const & move, Team const & other);
-static bool block1 (Team const & user, Team const & other);
-static bool block2 (Team const & user, Weather const & weather);
-}	// anonymous namespace
+
+bool is_blocked_by_bide (Team const & user);
+bool is_blocked_from_switching (Team const & user, Team const & other, Weather const & weather);
+bool imprison (Move const & move, Team const & other);
+bool blocked_by_torment (Team const & user);
+bool block1 (Team const & user, Team const & other);
+bool block2 (Team const & user, Weather const & weather);
+bool is_locked_in (Team const & user);
+void handle_sleep_counter (Team & user);
+void handle_confusion (Team & user);
+
+}	// unnamed namespace
 
 void block_selection (Team & user, Team const & other, Weather const & weather) {
 	user.pokemon().move().selectable = true;
-	if (user.bide and user.pokemon().move().name != Move::BIDE)
-			user.pokemon().move().selectable = false;
+	if (is_blocked_by_bide (user))
+		user.pokemon().move().selectable = false;
 	else if (user.pokemon().move().is_switch()) {
-		if (((other.pokemon().ability.blocks_switching (user, weather) or user.ingrain or user.trapped or user.partial_trap) and user.pokemon().item.name != Item::SHED_SHELL)
-				or (user.is_switching_to_self ()))
+		if (is_blocked_from_switching (user, other, weather) or user.is_switching_to_self ())
 			user.pokemon().move().selectable = false;
 	}
 	else if (user.pokemon().move().name == Move::STRUGGLE) {
-		for (Move const & move : user.pokemon().move.set) {
-			if (!move.is_struggle_or_switch () and move.selectable) {
+		for (auto it = user.pokemon().move.set.cbegin(); it->name != Move::STRUGGLE; ++it) {
+			if (it->selectable) {
 				// Struggle is only selectable if no other move is selectable
 				user.pokemon().move().selectable = false;
 				break;
 			}
 		}
 	}
-	else if ((block1 (user, other))
-			or (block2 (user, weather))
-			or (user.torment and user.pokemon().move().times_used != 0))
+	else if ((block1 (user, other)) or (block2 (user, weather)) or blocked_by_torment (user))
 		user.pokemon().move().selectable = false;
-	else if (user.encore or user.recharging or user.pokemon().item.is_choice_item ()) {
-		for (Move const & move : user.pokemon().move.set) {
-			if (move.name != user.pokemon().move().name and move.times_used != 0) {
+	else if (is_locked_in (user)) {
+		for (auto it = user.pokemon().move.set.cbegin(); it->name != Move::STRUGGLE; ++it) {
+			if (it->name != user.pokemon().move().name and it->times_used != 0) {
 				user.pokemon().move().selectable = false;
 				break;
 			}
@@ -65,58 +69,50 @@ void block_selection (Team & user, Team const & other, Weather const & weather) 
 }
 
 void block_execution (Team & user, Team const & other, Weather const & weather) {
-	if (!user.pokemon().move().is_switch()) {
-		if (user.pokemon().hp.stat == 0 or (other.pokemon().hp.stat == 0 and false))
+	if (user.pokemon().move().is_switch())
+		return;
+	if (user.pokemon().hp.stat == 0 or (other.pokemon().hp.stat == 0 and false))
+		user.pokemon().move().execute = false;
+	else {
+		if (user.pokemon().status.name == Status::FREEZE and !user.pokemon().move().is_usable_while_frozen ())
 			user.pokemon().move().execute = false;
-		else {
-			if (user.pokemon().status.name == Status::FREEZE and (!user.pokemon().move().is_usable_while_frozen ()))
-				user.pokemon().move().execute = false;
+		else if (user.pokemon().status.is_sleeping ())
+			handle_sleep_counter (user);
 
-			else if (user.pokemon().status.is_sleeping ()) {
-				if (user.awaken) {
-					user.pokemon().sleep = 0;
-					user.pokemon().status.clear ();
-				}
-				else {
-					if (user.pokemon().ability.name == Ability::EARLY_BIRD)
-						user.pokemon().sleep += 2;
-					else
-						++user.pokemon().sleep;
-					if (!user.pokemon().move().is_usable_while_sleeping ())
-						user.pokemon().move().execute = false;
-				}
-			}
+		if (block1 (user, other) or (user.pokemon().ability.name == Ability::TRUANT and user.loaf))
+			user.pokemon().move().execute = false;
 
-			if (block1 (user, other)
-					or (user.pokemon().ability.name == Ability::TRUANT and user.loaf))
-				user.pokemon().move().execute = false;
+		if (user.pokemon().move().execute and user.confused)
+			handle_confusion (user);
 
-			if (user.pokemon().move().execute and user.confused != 0) {
-				if (user.hitself) {
-					// fix
-					user.pokemon().move().execute = false;
-				}
-				else
-					--user.confused;
-			}
-			if (user.pokemon().move().execute and user.flinch) {
+		if (user.pokemon().move().execute) {
+			if (user.flinch) {
 				if (user.pokemon().ability.name == Ability::STEADFAST)
 					Stat::boost (user.stage [Stat::SPE], 1);
 				user.pokemon().move().execute = false;
 			}
-	
-			if (block2 (user, weather) or user.fully_paralyzed)
+			else if (block2 (user, weather) or user.fully_paralyzed) {
 				user.pokemon().move().execute = false;
-		
-			if (user.recharging) {
-				user.pokemon().move().execute = false;
-				user.recharging = false;
 			}
+		}
+	
+		if (user.recharging) {
+			user.pokemon().move().execute = false;
+			user.recharging = false;
 		}
 	}
 }
 
 namespace {
+
+bool is_blocked_by_bide (Team const & user) {
+	return user.bide and user.pokemon().move().name != Move::BIDE;
+}
+
+bool is_blocked_from_switching (Team const & user, Team const & other, Weather const & weather) {
+	return (other.pokemon().ability.blocks_switching (user, weather) or user.ingrain or user.trapped or user.partial_trap) and user.pokemon().item.name != Item::SHED_SHELL;
+}
+
 // Things that both block selection and block execution in between sleep and confusion
 bool block1 (Team const & user, Team const & other) {
 	return (user.pokemon().move().pp == 0)
@@ -138,8 +134,41 @@ bool imprison (Move const & move, Team const & other) {
 // Things that both block selection and block execution after flinching
 bool block2 (Team const & user, Weather const & weather) {
 	return (user.taunt and user.pokemon().move().basepower == 0)
-			or (weather.gravity and (user.pokemon().move().is_blocked_by_gravity ()));
+			or (weather.gravity and user.pokemon().move().is_blocked_by_gravity ());
 }
 
-}	// anonymous namespace
+bool is_locked_in (Team const & user) {
+	return user.encore or user.recharging or user.pokemon().item.is_choice_item ();
 }
+
+bool blocked_by_torment (Team const & user) {
+	return user.torment and user.pokemon().move().times_used != 0;
+}
+
+void handle_sleep_counter (Team & user) {
+	if (user.awaken) {
+		user.pokemon().sleep = 0;
+		user.pokemon().status.clear ();
+	}
+	else {
+		if (user.pokemon().ability.name == Ability::EARLY_BIRD)
+			user.pokemon().sleep += 2;
+		else
+			++user.pokemon().sleep;
+		if (!user.pokemon().move().is_usable_while_sleeping ())
+			user.pokemon().move().execute = false;
+	}
+}
+
+void handle_confusion (Team & user) {
+	if (user.hitself) {
+		// TODO: make user hit self in confusion
+		user.pokemon().move().execute = false;
+	}
+	else {
+		--user.confused;
+	}
+}
+
+}	// unnamed namespace
+}	// namespace technicalmachine
