c6df1b634a72d92bd938da3766201706660d1823
David Stone <david@doublewise.net>
2015-01-26 22:51:43 -0700

    Changed from std::vector<EV *> to std::vector<std::reference_wrapper<EV>>.

--- source/team_predictor/ev_optimizer/ev_optimizer.cpp
+++ source/team_predictor/ev_optimizer/ev_optimizer.cpp
@@ -18,18 +18,20 @@
 
 #include "ev_optimizer.hpp"
 
-#include <algorithm>
-#include <cstddef>
-#include <cstdint>
-#include <random>
-#include <utility>
-
 #include "combine.hpp"
 #include "defensive.hpp"
 #include "offensive.hpp"
 #include "speed.hpp"
 
 #include "../../pokemon/pokemon.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <random>
+#include <utility>
+#include <vector>
 
 namespace technicalmachine {
 namespace {
@@ -44,9 +46,7 @@ sum_type ev_sum(Pokemon const & pokemon) {
 	return std::accumulate(std::begin(regular), std::end(regular), sum_type(get_hp(pokemon).ev.value()), ev_sum);
 }
 
-void add_non_full_evs(std::vector<EV *> & evs, EV & ev);
-
-}	// unnamed namespace
+}	// namespace
 
 void optimize_evs(Pokemon & pokemon, std::mt19937 & random_engine) {
 	minimize_evs(pokemon);
@@ -69,10 +69,15 @@ void pad_random_evs(Pokemon & pokemon, std::mt19937 & random_engine) {
 	// create a uniform distribution of available stats non-maxed EVs. I repeat
 	// the process in case a stat is overfilled.
 	while (ev_sum(pokemon) < EV::max_total) {
-		std::vector<EV *> evs;
-		add_non_full_evs(evs, get_hp(pokemon).ev);
+		std::vector<std::reference_wrapper<EV>> evs;
+		auto const add_non_full_evs = [&](EV & ev) {
+			if (!is_maxed(ev)) {
+				evs.emplace_back(ev);
+			}
+		};
+		add_non_full_evs(get_hp(pokemon).ev);
 		for (auto const stat : regular_stats()) {
-			add_non_full_evs(evs, get_stat(pokemon, stat).ev);
+			add_non_full_evs(get_stat(pokemon, stat).ev);
 		}
 		auto const extra_evs = bounded::make<EV::max_total>() - ev_sum(pokemon);
 		static constexpr auto number_of_stats = 6;
@@ -82,25 +87,15 @@ void pad_random_evs(Pokemon & pokemon, std::mt19937 & random_engine) {
 		std::fill(std::begin(shuffled), std::begin(shuffled) + static_cast<int>(evs.size()) - 1, 0_bi);
 		std::shuffle(std::begin(shuffled), std::end(shuffled), random_engine);
 		auto it = shuffled.begin();
-		for (auto const & ev : evs) {
+		for (EV & ev : evs) {
 			auto const prior = it;
 			it = std::find(prior, std::end(shuffled), 0_bi);
 			// I use clamped here because I expect there to be some extra EVs
 			// assigned to some stats, which is why I put this in a loop.
-			ev->add(static_cast<bounded::clamped_integer<0, EV::max>>(std::distance(prior, it)));
+			ev.add(static_cast<bounded::clamped_integer<0, EV::max>>(std::distance(prior, it)));
 			++it;
 		}
 	}
 }
-
-namespace {
-
-void add_non_full_evs(std::vector<EV *> & evs, EV & ev) {
-	if (!is_maxed(ev)) {
-		evs.emplace_back(&ev);
-	}
-}
-
-}	// unnamed namespace
 
 }	// namespace technicalmachine
