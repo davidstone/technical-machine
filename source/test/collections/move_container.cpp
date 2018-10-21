b311052c174d15e2ea2442de5be1afbaa18c574d
David Stone <david@doublewise.net>
2018-10-21 11:34:29 -0600

    Simplified test/collections/move_container.

--- source/test/collections/move_container.cpp
+++ source/test/collections/move_container.cpp
@@ -1,5 +1,5 @@
 // Test container for moves
-// Copyright (C) 2014 David Stone
+// Copyright (C) 2018 David Stone
 //
 // This file is part of Technical Machine.
 //
@@ -18,7 +18,6 @@
 
 #include "move_container.hpp"
 
-#include "create_regular_moves.hpp"
 #include "create_shared_moves.hpp"
 #include "invalid_collection.hpp"
 
@@ -27,6 +26,7 @@
 
 #include "../../string_conversions/move.hpp"
 
+#include <containers/array/array.hpp>
 #include <containers/integer_range.hpp>
 
 #include <iostream>
@@ -37,13 +37,15 @@ namespace {
 using namespace bounded::literal;
 
 struct Verify {
-	Verify(containers::vector<Moves> m, TeamSize team_size):
+	Verify(containers::array<Moves, 4> m, TeamSize team_size):
 		m_moves(std::move(m)),
 		m_shared_moves(create_shared_moves(team_size)),
 		m_index(0_bi) {
 	}
 	void operator()(Moves const move) {
-		auto const mine = (m_index < size(m_moves)) ? at(m_moves, m_index) : at(m_shared_moves, m_index - size(m_moves));
+		auto const mine = (m_index < size(m_moves)) ?
+			at(m_moves, m_index) :
+			at(m_shared_moves, m_index - size(m_moves));
 		if (mine != move) {
 			throw InvalidCollection(
 				"MoveContainer has the wrong moves. Expected: " +
@@ -69,19 +71,21 @@ private:
 void move_container_tests() {
 	std::cout << "\tRunning MoveContainer tests.\n";
 	constexpr auto team_size = TeamSize(4_bi);
-	constexpr auto shared_moves_size = team_size + 1_bi;
+	constexpr auto shared_moves_size = team_size + number_of_weird_moves;
 	auto c = MoveContainer(team_size);
 	if (size(c) != shared_moves_size) {
 		throw InvalidCollection("MoveContainer has the wrong number of shared moves. Expecting " + bounded::to_string(shared_moves_size) + " but got " + bounded::to_string(size(c)));
 	}
-	auto const moves = create_regular_moves();
-	for (auto const n : containers::integer_range(static_cast<bounded::checked_integer<0, 100>>(size(moves)))) {
+	constexpr auto moves = containers::array{
+		Moves::Absorb, Moves::Tackle, Moves::Quick_Attack, Moves::Body_Slam
+	};
+	for (auto const n : containers::integer_range(size(moves))) {
 		c.emplace_back(moves[n]);
 		if (size(c) != shared_moves_size + n + 1_bi or size(c) != static_cast<bounded::checked_integer<0, 100>>(c.number_of_regular_moves()) + shared_moves_size) {
 			throw InvalidCollection("MoveContainer has the wrong number of moves during addition of moves.");
 		}
 	}
-	Verify verify(moves, team_size);
+	auto verify = Verify(moves, team_size);
 	for (auto const move : c) {
 		verify(move);
 	}
