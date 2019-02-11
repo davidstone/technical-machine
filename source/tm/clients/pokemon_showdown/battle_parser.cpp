3a96486e0a8230e41a43d46ca8ba4e03ad7d7b09
David Stone <david@doublewise.net>
2019-02-10 21:43:03 -0700

    Only log one level of analysis deep, or we end up with hundreds of megabytes per battle.

--- source/tm/clients/pokemon_showdown/battle_parser.cpp
+++ source/tm/clients/pokemon_showdown/battle_parser.cpp
@@ -556,7 +556,7 @@ Moves BattleParser::determine_action() {
 	auto predicted = predict_team(m_usage_stats, use_lead_stats, m_battle.foe(), m_random_engine);
 	m_analysis_logger << "Predicted " << to_string(predicted) << '\n' << std::flush;
 
-	return expectiminimax(m_battle.ai(), predicted, m_battle.weather(), m_evaluate, Depth(m_depth, 2U), m_analysis_logger);
+	return expectiminimax(m_battle.ai(), predicted, m_battle.weather(), m_evaluate, Depth(m_depth, 1U), m_analysis_logger);
 }
 
 void BattleParser::send_move(Moves const move) {
