6b7577d95fe49b8b4a1b757270288bae19abab50
David Stone <david@doublewise.net>
2019-03-22 17:41:02 -0600

    Added Generation enum.

---
+++ source/tm/generation.hpp
@@ -0,0 +1,30 @@
+// Copyright (C) 2019 David Stone
+//
+// This file is part of Technical Machine.
+//
+// Technical Machine is free software: you can redistribute it and / or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <tm/operators.hpp>
+
+namespace technicalmachine {
+
+enum class Generation { one, two, three, four, five, six, seven };
+
+constexpr auto compare(Generation const lhs, Generation const rhs) {
+	return compare(static_cast<int>(lhs), static_cast<int>(rhs));
+}
+
+} // namespace technicalmachine
