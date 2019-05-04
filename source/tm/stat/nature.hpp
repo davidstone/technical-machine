702c7afac0a96fd5f7ba49fd720104e9e0639739
David Stone <david@doublewise.net>
2019-05-04 17:12:26 -0600

    Simplified implementation of boosts_stat and lowers_stat for Nature. Moved all Nature code into header and declared constexpr.

--- source/tm/stat/nature.hpp
+++ source/tm/stat/nature.hpp
@@ -17,18 +17,19 @@
 
 #pragma once
 
-#include <cstdint>
-#include <bounded/integer.hpp>
-
 #include <tm/stat/stat_names.hpp>
 
 #include <tm/enum.hpp>
 #include <tm/rational.hpp>
 
+#include <bounded/integer.hpp>
+
+#include <cstdint>
+
 namespace technicalmachine {
 using namespace bounded::literal;
 
-enum class Nature : uint8_t {
+enum class Nature : std::uint8_t {
 	Adamant, Bashful, Bold, Brave, Calm,
 	Careful, Docile, Gentle, Hardy, Hasty,
 	Impish, Jolly, Lax, Lonely, Mild, Modest,
@@ -36,24 +37,102 @@ enum class Nature : uint8_t {
 	Relaxed, Sassy, Serious, Timid
 };
 
-auto boosts_stat(Nature nature, StatNames stat) -> bool;
-auto lowers_stat(Nature nature, StatNames stat) -> bool;
+constexpr auto boosts_stat(Nature const nature, StatNames const stat) -> bool {
+	switch (nature) {
+		case Nature::Adamant:
+		case Nature::Brave:
+		case Nature::Lonely:
+		case Nature::Naughty:
+			return stat == StatNames::ATK;
+		case Nature::Bold:
+		case Nature::Impish:
+		case Nature::Lax:
+		case Nature::Relaxed:
+			return stat == StatNames::DEF;
+		case Nature::Mild:
+		case Nature::Modest:
+		case Nature::Quiet:
+		case Nature::Rash:
+			return stat == StatNames::SPA;
+		case Nature::Calm:
+		case Nature::Careful:
+		case Nature::Gentle:
+		case Nature::Sassy:
+			return stat == StatNames::SPD;
+		case Nature::Hasty:
+		case Nature::Jolly:
+		case Nature::Naive:
+		case Nature::Timid:
+			return stat == StatNames::SPE;
+		case Nature::Bashful:
+		case Nature::Docile:
+		case Nature::Hardy:
+		case Nature::Quirky:
+		case Nature::Serious:
+			return false;
+	}
+}
 
-inline auto boost(Nature const nature, StatNames const stat) {
+constexpr auto lowers_stat(Nature const nature, StatNames const stat) -> bool {
+	switch (nature) {
+		case Nature::Bold:
+		case Nature::Calm:
+		case Nature::Modest:
+		case Nature::Timid:
+			return stat == StatNames::ATK;
+		case Nature::Gentle:
+		case Nature::Hasty:
+		case Nature::Lonely:
+		case Nature::Mild:
+			return stat == StatNames::DEF;
+		case Nature::Adamant:
+		case Nature::Careful:
+		case Nature::Impish:
+		case Nature::Jolly:
+			return stat == StatNames::SPA;
+		case Nature::Lax:
+		case Nature::Naive:
+		case Nature::Naughty:
+		case Nature::Rash:
+			return stat == StatNames::SPD;
+		case Nature::Brave:
+		case Nature::Quiet:
+		case Nature::Relaxed:
+		case Nature::Sassy:
+			return stat == StatNames::SPE;
+		case Nature::Bashful:
+		case Nature::Docile:
+		case Nature::Hardy:
+		case Nature::Quirky:
+		case Nature::Serious:
+			return false;
+	}
+}
+
+constexpr auto boosts_attacking_stat(Nature const nature) -> bool {
+	return boosts_stat(nature, StatNames::ATK) or boosts_stat(nature, StatNames::SPA);
+}
+
+constexpr auto boosts_defending_stat(Nature const nature) -> bool {
+	return boosts_stat(nature, StatNames::DEF) or boosts_stat(nature, StatNames::SPD);
+}
+
+constexpr auto lowers_attacking_stat(Nature const nature) -> bool {
+	return lowers_stat(nature, StatNames::ATK) or lowers_stat(nature, StatNames::SPA);
+}
+
+constexpr auto lowers_defending_stat(Nature const nature) -> bool {
+	return lowers_stat(nature, StatNames::DEF) or lowers_stat(nature, StatNames::SPD);
+}
+
+constexpr auto boost(Nature const nature, StatNames const stat) {
 	auto const numerator =
 		BOUNDED_CONDITIONAL(boosts_stat(nature, stat), 11_bi,
 		BOUNDED_CONDITIONAL(lowers_stat(nature, stat), 9_bi,
 		10_bi
 	));
-	static constexpr auto denominator = 10_bi;
-	return rational(numerator, denominator);
+	return rational(numerator, 10_bi);
 }
-
-auto boosts_attacking_stat(Nature nature) -> bool;
-auto boosts_defending_stat(Nature nature) -> bool;
-auto lowers_attacking_stat(Nature nature) -> bool;
-auto lowers_defending_stat(Nature nature) -> bool;
-
 
 }	// namespace technicalmachine
 
