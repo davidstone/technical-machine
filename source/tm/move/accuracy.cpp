20ef732bc14ff77ea72316a80e05c0b762087332
David Stone <david@doublewise.net>
2020-01-26 11:56:09 -0700

    Temporary change to work around clang bug

--- source/tm/move/accuracy.cpp
+++ source/tm/move/accuracy.cpp
@@ -27,6 +27,8 @@
 namespace technicalmachine {
 using namespace bounded::literal;
 
+// TODO: Simplify some of these after fix for
+// https://bugs.llvm.org/show_bug.cgi?id=44666
 auto accuracy(Generation const generation, Moves const move) -> BaseAccuracy {
 	using bounded::none;
 	switch (move) {
@@ -94,7 +96,7 @@ auto accuracy(Generation const generation, Moves const move) -> BaseAccuracy {
 		case Moves::Leer: return 100_bi;
 		case Moves::Bite: return 100_bi;
 		case Moves::Growl: return 100_bi;
-		case Moves::Roar: return BOUNDED_CONDITIONAL(generation <= Generation::five, 100_bi, none);
+		case Moves::Roar: return bounded::optional<bounded::integer<30, 100>>(BOUNDED_CONDITIONAL(generation <= Generation::five, 100_bi, none));
 		case Moves::Sing: return 55_bi;
 		case Moves::Supersonic: return 55_bi;
 		case Moves::Sonic_Boom: return 90_bi;
@@ -160,7 +162,7 @@ auto accuracy(Generation const generation, Moves const move) -> BaseAccuracy {
 		case Moves::Rage: return 100_bi;
 		case Moves::Teleport: return none;
 		case Moves::Night_Shade: return 100_bi;
-		case Moves::Mimic: return BOUNDED_CONDITIONAL(generation <= Generation::two, 100_bi, none);
+		case Moves::Mimic: return bounded::optional<bounded::integer<30, 100>>(BOUNDED_CONDITIONAL(generation <= Generation::two, 100_bi, none));
 		case Moves::Screech: return 85_bi;
 		case Moves::Double_Team: return none;
 		case Moves::Recover: return none;
@@ -254,12 +256,12 @@ auto accuracy(Generation const generation, Moves const move) -> BaseAccuracy {
 		case Moves::Super_Fang: return 90_bi;
 		case Moves::Slash: return 100_bi;
 		case Moves::Substitute: return none;
-		case Moves::Struggle: return BOUNDED_CONDITIONAL(generation <= Generation::three, 100_bi, none);
+		case Moves::Struggle: return bounded::optional<bounded::integer<30, 100>>(BOUNDED_CONDITIONAL(generation <= Generation::three, 100_bi, none));
 		case Moves::Sketch: return none;
 		case Moves::Triple_Kick: return 90_bi;
 		case Moves::Thief: return 100_bi;
 		case Moves::Spider_Web: return none;
-		case Moves::Mind_Reader: return BOUNDED_CONDITIONAL(generation <= Generation::three, 100_bi, none);
+		case Moves::Mind_Reader: return bounded::optional<bounded::integer<30, 100>>(BOUNDED_CONDITIONAL(generation <= Generation::three, 100_bi, none));
 		case Moves::Nightmare: return 100_bi;
 		case Moves::Flame_Wheel: return 100_bi;
 		case Moves::Snore: return 100_bi;
@@ -282,13 +284,13 @@ auto accuracy(Generation const generation, Moves const move) -> BaseAccuracy {
 		case Moves::Octazooka: return 85_bi;
 		case Moves::Spikes: return none;
 		case Moves::Zap_Cannon: return 50_bi;
-		case Moves::Foresight: return BOUNDED_CONDITIONAL(generation <= Generation::three, 100_bi, none);
+		case Moves::Foresight: return bounded::optional<bounded::integer<30, 100>>(BOUNDED_CONDITIONAL(generation <= Generation::three, 100_bi, none));
 		case Moves::Destiny_Bond: return none;
 		case Moves::Perish_Song: return none;
 		case Moves::Icy_Wind: return 95_bi;
 		case Moves::Detect: return none;
 		case Moves::Bone_Rush: return BOUNDED_CONDITIONAL(generation <= Generation::four, 80_bi, 90_bi);
-		case Moves::Lock_On: return BOUNDED_CONDITIONAL(generation <= Generation::three, 100_bi, none);
+		case Moves::Lock_On: return bounded::optional<bounded::integer<30, 100>>(BOUNDED_CONDITIONAL(generation <= Generation::three, 100_bi, none));
 		case Moves::Outrage: return 100_bi;
 		case Moves::Sandstorm: return none;
 		case Moves::Giga_Drain: return 100_bi;
@@ -309,7 +311,7 @@ auto accuracy(Generation const generation, Moves const move) -> BaseAccuracy {
 		case Moves::Present: return 90_bi;
 		case Moves::Frustration: return 100_bi;
 		case Moves::Safeguard: return none;
-		case Moves::Pain_Split: return BOUNDED_CONDITIONAL(generation <= Generation::two, 100_bi, none);
+		case Moves::Pain_Split: return bounded::optional<bounded::integer<30, 100>>(BOUNDED_CONDITIONAL(generation <= Generation::two, 100_bi, none));
 		case Moves::Sacred_Fire: return 95_bi;
 		case Moves::Magnitude: return 100_bi;
 		case Moves::Dynamic_Punch: return 50_bi;
@@ -405,7 +407,7 @@ auto accuracy(Generation const generation, Moves const move) -> BaseAccuracy {
 		case Moves::Fake_Tears: return 100_bi;
 		case Moves::Air_Cutter: return 95_bi;
 		case Moves::Overheat: return 90_bi;
-		case Moves::Odor_Sleuth: return BOUNDED_CONDITIONAL(generation <= Generation::three, 100_bi, none);
+		case Moves::Odor_Sleuth: return bounded::optional<bounded::integer<30, 100>>(BOUNDED_CONDITIONAL(generation <= Generation::three, 100_bi, none));
 		case Moves::Rock_Tomb: return BOUNDED_CONDITIONAL(generation <= Generation::five, 80_bi, 95_bi);
 		case Moves::Silver_Wind: return 100_bi;
 		case Moves::Metal_Sound: return 85_bi;
@@ -665,7 +667,7 @@ auto accuracy(Generation const generation, Moves const move) -> BaseAccuracy {
 		case Moves::Freeze_Dry: return 100_bi;
 		case Moves::Disarming_Voice: return none;
 		case Moves::Parting_Shot: return 100_bi;
-		case Moves::Topsy_Turvy: return BOUNDED_CONDITIONAL(generation <= Generation::six, 100_bi, none);
+		case Moves::Topsy_Turvy: return bounded::optional<bounded::integer<30, 100>>(BOUNDED_CONDITIONAL(generation <= Generation::six, 100_bi, none));
 		case Moves::Draining_Kiss: return 100_bi;
 		case Moves::Crafty_Shield: return none;
 		case Moves::Flower_Shield: return none;
