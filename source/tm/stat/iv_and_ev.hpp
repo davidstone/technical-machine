51f2498c00cde27610e770b3aee0484e80a8dc8f
David Stone <david@doublewise.net>
2020-06-07 11:23:08 -0600

    Add `IVAndEV`

---
+++ source/tm/stat/iv_and_ev.hpp
@@ -0,0 +1,32 @@
+// Copyright (C) 2020 David Stone
+//
+// This file is part of Technical Machine.
+//
+// Technical Machine is free software: you can redistribute it and / or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <tm/stat/ev.hpp>
+#include <tm/stat/iv.hpp>
+
+namespace technicalmachine {
+
+struct IVAndEV {
+	IV iv;
+	EV ev;
+
+	friend auto operator==(IVAndEV const &, IVAndEV const &) -> bool = default;
+};
+
+} // namespace technicalmachine
