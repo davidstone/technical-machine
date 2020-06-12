bcd8758edf93e6c33d30f8e06d67b99ad9d1d784
David Stone <david@doublewise.net>
2020-06-11 20:30:18 -0600

    Add lower-level version of has_physical_move for use while building up a Pokemon

--- source/tm/pokemon/has_physical_or_special_move.cpp
+++ source/tm/pokemon/has_physical_or_special_move.cpp
@@ -21,27 +21,50 @@
 #include <tm/move/known_move.hpp>
 
 #include <containers/algorithms/all_any_none.hpp>
+#include <containers/algorithms/transform.hpp>
 
 namespace technicalmachine {
 namespace {
 
-auto any_move_matches(Generation const generation, Pokemon const pokemon, auto const condition) -> bool {
-	return containers::any(regular_moves(pokemon), [=](Move const move) {
+auto any_move_matches(Generation const generation, containers::static_vector<Moves, max_moves_per_pokemon.value()> const moves, bounded::optional<Type> const hidden_power_type, auto const condition) -> bool {
+	return containers::any(moves, [=](Moves const move) {
 		return condition(
 			generation,
-			KnownMove{move.name(), get_type(generation, move.name(), get_hidden_power_type(pokemon))}
+			KnownMove{move, get_type(generation, move, hidden_power_type)}
 		);
 	});
 }
 
+auto get_move_names(Pokemon const pokemon) {
+	return containers::static_vector<Moves, max_moves_per_pokemon.value()>(
+		containers::transform(regular_moves(pokemon), [](Move const move) {
+			return move.name();
+		})
+	);
+}
+
 } // namespace
 
+auto has_physical_move(Generation const generation, containers::static_vector<Moves, max_moves_per_pokemon.value()> const moves, bounded::optional<Type> const hidden_power_type) -> bool {
+	return any_move_matches(generation, moves, hidden_power_type, is_physical);
+}
+
 auto has_physical_move(Generation const generation, Pokemon const pokemon) -> bool {
-	return any_move_matches(generation, pokemon, is_physical);
+	return any_move_matches(
+		generation,
+		get_move_names(pokemon),
+		get_hidden_power_type(pokemon),
+		is_physical
+	);
 }
 
 auto has_special_move(Generation const generation, Pokemon const pokemon) -> bool {
-	return any_move_matches(generation, pokemon, is_special);
+	return any_move_matches(
+		generation,
+		get_move_names(pokemon),
+		get_hidden_power_type(pokemon),
+		is_special
+	);
 }
 
 } // namespace technicalmachine
