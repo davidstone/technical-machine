4b29bafb1a04c9fd443e4e7b2e97e52ca6125232
David Stone <david@doublewise.net>
2020-07-14 20:53:02 -0600

    Moved BestMove into its own file

--- source/tm/evaluate/transposition.hpp
+++ source/tm/evaluate/transposition.hpp
@@ -17,6 +17,8 @@
 
 #pragma once
 
+#include <tm/evaluate/best_move.hpp>
+
 #include <tm/move/moves.hpp>
 
 #include <tm/compress.hpp>
@@ -29,11 +31,6 @@
 namespace technicalmachine {
 
 struct Team;
-
-struct BestMove {
-	Moves name;
-	double score;
-};
 
 struct TranspositionTable {
 	auto add_score(Team const & ai, Team const & foe, Weather, unsigned depth, BestMove) -> void;
