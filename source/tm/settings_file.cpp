6d2d423556550598a13d610bd75af0e8eb02b8e8
David Stone <davidfromonline@gmail.com>
2021-04-28 17:52:04 -0600

    Use `nlohmann::json` for SettingsFile

--- source/tm/settings_file.cpp
+++ source/tm/settings_file.cpp
@@ -6,28 +6,32 @@
 
 #include <tm/settings_file.hpp>
 
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
-
 #include <containers/string.hpp>
+
+#include <nlohmann/json.hpp>
+
+#include <fstream>
 
 namespace technicalmachine {
 
 auto load_settings_file(std::filesystem::path const & path) -> SettingsFile {
-	boost::property_tree::ptree pt;
-	read_json(path.string(), pt);
-	auto const & server = pt.get_child("settings");
-	auto username = containers::string(server.get<std::string>("username"));
+	auto file = std::ifstream(path);
+	auto json = nlohmann::json();
+	file >> json;
+	auto const & server = json.at("settings");
+	auto username = containers::string(server.at("username").get<std::string_view>());
 	if (containers::is_empty(username)) {
 		throw std::runtime_error("Missing username and password in settings file");
 	}
-	auto team_file = server.get<std::string>("team");
+	auto team_file = server.at("team").get<std::string_view>();
 
 	return SettingsFile{
 		BOUNDED_CONDITIONAL(!containers::is_empty(team_file), std::filesystem::path(team_file), bounded::none),
+		containers::string(server.at("host").get<std::string_view>()),
+		containers::string(server.at("port").get<std::string_view>()),
 		std::move(username),
-		containers::string(server.get<std::string>("password")),
-		containers::string(server.get<std::string>("resource"))
+		containers::string(server.at("password").get<std::string_view>()),
+		containers::string(server.at("resource").get<std::string_view>())
 	};
 }
 
