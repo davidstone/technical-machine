18ab52f0439e44326ce491cb50d94e72f01229da
David Stone <davidfromonline@gmail.com>
2021-05-18 07:34:04 -0600

    Fix indentation

--- source/tm/pokemon/pokemon.cpp
+++ source/tm/pokemon/pokemon.cpp
@@ -11,77 +11,77 @@ namespace technicalmachine {
 
 template<Generation generation>
 Pokemon<generation>::Pokemon(Species const species, Level const level, Gender const gender, Item const item, Ability const ability, CombinedStats<IVAndEV> stats, RegularMoves regular_moves_, Happiness const happiness):
-    m_regular_moves(regular_moves_),
-    m_stats(BaseStats(generation, species), stats, level),
+	m_regular_moves(regular_moves_),
+	m_stats(BaseStats(generation, species), stats, level),
 
-    m_species(species),
-    m_item(item),
-    m_ability(ability),
-    m_gender(gender),
-    m_nature(stats.nature),
+	m_species(species),
+	m_item(item),
+	m_ability(ability),
+	m_gender(gender),
+	m_nature(stats.nature),
 
-    m_level(level),
+	m_level(level),
 
-    m_happiness(happiness),
+	m_happiness(happiness),
 
-    // TODO: Make this none if there is no way to call Hidden Power
-    // TODO: Use the IVs provided
-    m_hidden_power([=]{
-        if constexpr (generation == Generation::one) {
-            return bounded::none;
-        } else if constexpr (generation == Generation::two) {
-            constexpr auto dv = DV(15_bi);
-            return HiddenPower<generation>(DVs{dv, dv, dv, dv});
-        } else {
-            constexpr auto iv = IV(31_bi);
-            return HiddenPower<generation>(IVs{iv, iv, iv, iv, iv, iv});
-        }
-    }()),
-    
-    m_has_been_seen(false),
+	// TODO: Make this none if there is no way to call Hidden Power
+	// TODO: Use the IVs provided
+	m_hidden_power([=]{
+		if constexpr (generation == Generation::one) {
+			return bounded::none;
+		} else if constexpr (generation == Generation::two) {
+			constexpr auto dv = DV(15_bi);
+			return HiddenPower<generation>(DVs{dv, dv, dv, dv});
+		} else {
+			constexpr auto iv = IV(31_bi);
+			return HiddenPower<generation>(IVs{iv, iv, iv, iv, iv, iv});
+		}
+	}()),
+	
+	m_has_been_seen(false),
 
-    m_ability_is_known(true),
-    m_item_is_known(true),
-    m_nature_is_known(true)
+	m_ability_is_known(true),
+	m_item_is_known(true),
+	m_nature_is_known(true)
 {
 }
 
 template<Generation generation>
 Pokemon<generation>::Pokemon(Species const species, Level const level, Gender const gender) : 
-    Pokemon::Pokemon(
-        species,
-        level,
-        gender,
-        Item::None,
-        Ability::Honey_Gather,
-        CombinedStats<IVAndEV>{
-            Nature::Hardy,
-            {default_iv(generation), EV(0_bi)},
-            {default_iv(generation), EV(0_bi)},
-            {default_iv(generation), EV(0_bi)},
-            {default_iv(generation), EV(0_bi)},
-            {default_iv(generation), EV(0_bi)},
-            {default_iv(generation), EV(0_bi)},
-        },
-        RegularMoves(),
-        Happiness()
-    )
+	Pokemon::Pokemon(
+		species,
+		level,
+		gender,
+		Item::None,
+		Ability::Honey_Gather,
+		CombinedStats<IVAndEV>{
+			Nature::Hardy,
+			{default_iv(generation), EV(0_bi)},
+			{default_iv(generation), EV(0_bi)},
+			{default_iv(generation), EV(0_bi)},
+			{default_iv(generation), EV(0_bi)},
+			{default_iv(generation), EV(0_bi)},
+			{default_iv(generation), EV(0_bi)},
+		},
+		RegularMoves(),
+		Happiness()
+	)
 {
-    m_ability_is_known = false;
-    m_item_is_known = false;
-    m_nature_is_known = false;
+	m_ability_is_known = false;
+	m_item_is_known = false;
+	m_nature_is_known = false;
 }
 
 template<Generation generation>
 auto Pokemon<generation>::set_ev(PermanentStat const stat_name, IV const iv, EV const ev) -> void {
-    auto const base_stats = BaseStats(generation, species());
-    if (stat_name == PermanentStat::hp) {
-        m_stats.hp() = HP(base_stats, level(), iv, ev);
-    } else {
-        auto const regular = RegularStat(stat_name);
-        auto & stat = m_stats[regular];
-        stat = initial_stat(regular, base_stats[regular], nature(), iv, ev, level());
-    }
+	auto const base_stats = BaseStats(generation, species());
+	if (stat_name == PermanentStat::hp) {
+		m_stats.hp() = HP(base_stats, level(), iv, ev);
+	} else {
+		auto const regular = RegularStat(stat_name);
+		auto & stat = m_stats[regular];
+		stat = initial_stat(regular, base_stats[regular], nature(), iv, ev, level());
+	}
 }
 
 template struct Pokemon<Generation::one>;
