c9bbe0bcc4d87164dd5b836e3bcedfcae1e3b372
David Stone <davidfromonline@gmail.com>
2021-06-18 04:07:25 -0600

    Add a test for `initial_stat`

--- source/tm/stat/initial_stat.cpp
+++ source/tm/stat/initial_stat.cpp
@@ -4,3 +4,11 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <tm/stat/initial_stat.hpp>
+
+namespace technicalmachine {
+namespace {
+
+static_assert(initial_stat<Generation::three>(SplitSpecialRegularStat::spe, 100_bi, Level(100_bi), Nature::Hardy, IV(31_bi), EV(252_bi)) == 299_bi);
+
+} // namespace
+} // namespace technicalmachine
