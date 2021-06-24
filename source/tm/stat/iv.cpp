6d6943589f39e7302ca1ad196cfc38480034f899
David Stone <davidfromonline@gmail.com>
2021-06-24 06:56:50 -0600

    Add `DV` tests

--- source/tm/stat/iv.cpp
+++ source/tm/stat/iv.cpp
@@ -5,3 +5,14 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <tm/stat/iv.hpp>
+
+namespace technicalmachine {
+
+using namespace bounded::literal;
+
+static_assert(DVs(DV(15_bi), DV(15_bi), DV(15_bi), DV(15_bi)).hp() == DV(15_bi));
+static_assert(DVs(DV(15_bi), DV(14_bi), DV(15_bi), DV(15_bi)).hp() == DV(11_bi));
+static_assert(DVs(DV(15_bi), DV(13_bi), DV(15_bi), DV(15_bi)).hp() == DV(15_bi));
+static_assert(DVs(DV(14_bi), DV(14_bi), DV(14_bi), DV(14_bi)).hp() == DV(0_bi));
+
+} // namespace technicalmachine
