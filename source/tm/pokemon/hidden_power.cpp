72a32772d7ffa0c8414a257d7e07998a1fd8204a
David Stone <davidfromonline@gmail.com>
2021-07-01 09:14:26 -0600

    Add more `HiddenPower` tests

--- source/tm/pokemon/hidden_power.cpp
+++ source/tm/pokemon/hidden_power.cpp
@@ -239,6 +239,32 @@ constexpr bool test() {
 		static_assert(hp.type() == Type::Water);
 	}
 
+	{
+		constexpr auto hp = HiddenPower<Generation::three>(IVs(
+			IV(29_bi),
+			IV(31_bi),
+			IV(30_bi),
+			IV(29_bi),
+			IV(30_bi),
+			IV(29_bi)
+		));
+		static_assert(hp.power() == 54_bi);
+		static_assert(hp.type() == Type::Ghost);
+	}
+
+	{
+		constexpr auto hp = HiddenPower<Generation::four>(IVs(
+			IV(31_bi),
+			IV(3_bi),
+			IV(31_bi),
+			IV(31_bi),
+			IV(31_bi),
+			IV(31_bi)
+		));
+		static_assert(hp.power() == 70_bi);
+		static_assert(hp.type() == Type::Dark);
+	}
+
 	test_impl<Generation::two>();
 	test_impl<Generation::three>();
 
