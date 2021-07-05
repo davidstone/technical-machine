eed6b3746762229f633b61784a5b0f623da35dd5
David Stone <davidfromonline@gmail.com>
2021-07-05 06:17:04 -0600

    Add some `initial_stat` unit tests

--- source/tm/stat/initial_stat.cpp
+++ source/tm/stat/initial_stat.cpp
@@ -10,5 +10,11 @@ namespace {
 
 static_assert(initial_stat<Generation::three>(SplitSpecialRegularStat::spe, 100_bi, Level(100_bi), Nature::Hardy, IV(31_bi), EV(252_bi)) == 299_bi);
 
+static_assert(initial_stat<Generation::four>(SplitSpecialRegularStat::atk, 65_bi, Level(1_bi), Nature::Hardy, IV(31_bi), EV(0_bi)) == 6_bi);
+static_assert(initial_stat<Generation::four>(SplitSpecialRegularStat::def, 60_bi, Level(1_bi), Nature::Hardy, IV(31_bi), EV(196_bi)) == 7_bi);
+static_assert(initial_stat<Generation::four>(SplitSpecialRegularStat::spa, 130_bi, Level(1_bi), Nature::Hardy, IV(31_bi), EV(36_bi)) == 8_bi);
+static_assert(initial_stat<Generation::four>(SplitSpecialRegularStat::spd, 75_bi, Level(1_bi), Nature::Hardy, IV(31_bi), EV(76_bi)) == 7_bi);
+static_assert(initial_stat<Generation::four>(SplitSpecialRegularStat::spe, 110_bi, Level(1_bi), Nature::Hardy, IV(31_bi), EV(196_bi)) == 8_bi);
+
 } // namespace
 } // namespace technicalmachine
