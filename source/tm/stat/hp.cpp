6e61ff17ac3cfc9274a490d58a60a80874435534
David Stone <davidfromonline@gmail.com>
2021-07-05 06:16:52 -0600

    Add some `HP` unit tests

--- source/tm/stat/hp.cpp
+++ source/tm/stat/hp.cpp
@@ -4,3 +4,13 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <tm/stat/hp.hpp>
+
+namespace technicalmachine {
+namespace {
+
+static_assert(HP(100_bi).max() == 100_bi);
+static_assert(HP(100_bi).current() == 100_bi);
+static_assert(HP(60_bi, Level(1_bi), IV(31_bi), EV(0_bi)).max() == 12_bi);
+
+} // namespace
+} // namespace technicalmachine
