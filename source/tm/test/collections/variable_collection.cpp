ecf5c5ec4825bf2bb33a86c3d00c21335af56714
David Stone <davidfromonline@gmail.com>
2023-04-07 10:28:17 -0600

    Run phaze tests at compile time

--- source/tm/test/collections/variable_collection.cpp
+++ source/tm/test/collections/variable_collection.cpp
@@ -3,8 +3,11 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#include <compare>
-#include <catch2/catch_test_macros.hpp>
+module;
+
+#include <bounded/assert.hpp>
+
+export module tm.test.collections.variable_collection;
 
 import tm.pokemon.pokemon_collection;
 
@@ -31,12 +34,11 @@ import bounded;
 import containers;
 
 namespace technicalmachine {
-namespace {
 using namespace bounded::literal;
 
 constexpr auto generation = Generation::four;
 
-auto make_team(auto const... species) {
+constexpr auto make_team(auto const... species) {
 	return Team<generation>({
 		Pokemon<generation>(
 			species,
@@ -52,7 +54,7 @@ auto make_team(auto const... species) {
 
 using EffectIndex = bounded::integer<0, 4>;
 template<Generation generation>
-void validate(Team<generation> const & team, EffectIndex const effect_index, TeamIndex const current_index) {
+constexpr auto validate(Team<generation> const & team, EffectIndex const effect_index, TeamIndex const current_index) -> void {
 	static constexpr auto expected_index = containers::array<bounded::integer<0, 5>, 6_bi, 5_bi>{{
 		{1_bi, 2_bi, 3_bi, 4_bi, 5_bi},
 		{0_bi, 2_bi, 3_bi, 4_bi, 5_bi},
@@ -64,56 +66,46 @@ void validate(Team<generation> const & team, EffectIndex const effect_index, Tea
 
 	auto const expected = expected_index[current_index][effect_index];
 	auto const calculated = team.all_pokemon().index();
-	CHECK(expected == calculated);
+	BOUNDED_ASSERT(expected == calculated);
 }
 
-auto test_phaze(Team<generation> user, Team<generation> team) {
+constexpr auto test_phaze(Team<generation> user, Team<generation> team) -> bool {
 	auto environment = Environment();
 	for (auto const current_index : containers::integer_range(team.size())) {
 		team.all_pokemon().set_index(current_index);
 		auto const side_effects = possible_side_effects(MoveName::Whirlwind, user.pokemon().as_const(), team, environment);
 		auto const expected_size = bounded::increase_min<0>(team.size() - 1_bi);
-		CHECK(containers::size(side_effects) == expected_size);
+		BOUNDED_ASSERT(containers::size(side_effects) == expected_size);
 		for (auto const effect_index : containers::integer_range(expected_size)) {
 			auto const & side_effect = side_effects[effect_index];
-			CHECK(side_effect.probability == 1.0 / double(team.size() - 1_bi));
+			BOUNDED_ASSERT(side_effect.probability == 1.0 / double(team.size() - 1_bi));
 			team.all_pokemon().set_index(current_index);
 			side_effect.function(user, team, environment, 0_bi);
 			validate(team, EffectIndex(effect_index), current_index);
 		}
 	}
+	return true;
 }
 
-TEST_CASE("Phaze 2 Pokemon", "[side effects]") {
-	test_phaze(
-		make_team(Species::Lugia),
-		make_team(Species::Bulbasaur, Species::Ivysaur)
-	);
-}
-TEST_CASE("Phaze 3 Pokemon", "[side effects]") {
-	test_phaze(
-		make_team(Species::Lugia),
-		make_team(Species::Bulbasaur, Species::Ivysaur, Species::Venusaur)
-	);
-}
-TEST_CASE("Phaze 4 Pokemon", "[side effects]") {
-	test_phaze(
-		make_team(Species::Lugia),
-		make_team(Species::Bulbasaur, Species::Ivysaur, Species::Venusaur, Species::Charmander)
-	);
-}
-TEST_CASE("Phaze 5 Pokemon", "[side effects]") {
-	test_phaze(
-		make_team(Species::Lugia),
-		make_team(Species::Bulbasaur, Species::Ivysaur, Species::Venusaur, Species::Charmander, Species::Charmeleon)
-	);
-}
-TEST_CASE("Phaze 6 Pokemon", "[side effects]") {
-	test_phaze(
-		make_team(Species::Lugia),
-		make_team(Species::Bulbasaur, Species::Ivysaur, Species::Venusaur, Species::Charmander, Species::Charmeleon, Species::Charizard)
-	);
-}
+static_assert(test_phaze(
+	make_team(Species::Lugia),
+	make_team(Species::Bulbasaur, Species::Ivysaur)
+));
+static_assert(test_phaze(
+	make_team(Species::Lugia),
+	make_team(Species::Bulbasaur, Species::Ivysaur, Species::Venusaur)
+));
+static_assert(test_phaze(
+	make_team(Species::Lugia),
+	make_team(Species::Bulbasaur, Species::Ivysaur, Species::Venusaur, Species::Charmander)
+));
+static_assert(test_phaze(
+	make_team(Species::Lugia),
+	make_team(Species::Bulbasaur, Species::Ivysaur, Species::Venusaur, Species::Charmander, Species::Charmeleon)
+));
+static_assert(test_phaze(
+	make_team(Species::Lugia),
+	make_team(Species::Bulbasaur, Species::Ivysaur, Species::Venusaur, Species::Charmander, Species::Charmeleon, Species::Charizard)
+));
 
-} // namespace
 } // namespace technicalmachine
