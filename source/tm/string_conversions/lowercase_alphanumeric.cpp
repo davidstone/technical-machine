f06f42ed3dc559b4509ad9e0ece2abd2c514baad
David Stone <davidfromonline@gmail.com>
2023-04-09 12:43:10 -0600

    Do not use a lambda when a normal function would work

--- source/tm/string_conversions/lowercase_alphanumeric.cpp
+++ source/tm/string_conversions/lowercase_alphanumeric.cpp
@@ -10,33 +10,35 @@ import std_module;
 
 namespace technicalmachine {
 
-export constexpr auto lowercase_alphanumeric = [](std::string_view input) {
-	// Not portable because it does not respect character encodings.
-	// We do not want to use cctype functions because we do not want to
-	// use locales.
-	auto to_lower = [](char c) {
-		return static_cast<char>(('A' <= c and c <= 'Z') ? c + 'a' - 'A' : c);
-	};
-	static_assert(to_lower('A') == 'a');
-	static_assert(to_lower('B') == 'b');
-	static_assert(to_lower('Z') == 'z');
-	static_assert(to_lower('a') == 'a');
-	static_assert(to_lower('b') == 'b');
-	static_assert(to_lower('0') == '0');
-	auto is_valid = [](char c) {
-		return ('0' <= c and c <= '9') or ('a' <= c and c <= 'z');
-	};
-	static_assert(is_valid('0'));
-	static_assert(is_valid('1'));
-	static_assert(is_valid('9'));
-	static_assert(is_valid('a'));
-	static_assert(is_valid('b'));
-	static_assert(is_valid('z'));
-	static_assert(!is_valid('A'));
-	static_assert(!is_valid('D'));
-	static_assert(!is_valid('Z'));
-	static_assert(!is_valid('%'));
-	static_assert(!is_valid(' '));
+// Not portable because it does not respect character encodings.
+// We do not want to use cctype functions because we do not want to
+// use locales.
+constexpr auto to_lower(char const c) -> char {
+	return static_cast<char>(('A' <= c and c <= 'Z') ? c + 'a' - 'A' : c);
+};
+static_assert(to_lower('A') == 'a');
+static_assert(to_lower('B') == 'b');
+static_assert(to_lower('Z') == 'z');
+static_assert(to_lower('a') == 'a');
+static_assert(to_lower('b') == 'b');
+static_assert(to_lower('0') == '0');
+
+constexpr auto is_valid(char const c) -> bool {
+	return ('0' <= c and c <= '9') or ('a' <= c and c <= 'z');
+};
+static_assert(is_valid('0'));
+static_assert(is_valid('1'));
+static_assert(is_valid('9'));
+static_assert(is_valid('a'));
+static_assert(is_valid('b'));
+static_assert(is_valid('z'));
+static_assert(!is_valid('A'));
+static_assert(!is_valid('D'));
+static_assert(!is_valid('Z'));
+static_assert(!is_valid('%'));
+static_assert(!is_valid(' '));
+
+export constexpr auto lowercase_alphanumeric = [](std::string_view const input) {
 	return containers::filter(containers::transform(std::move(input), to_lower), is_valid);
 };
 
