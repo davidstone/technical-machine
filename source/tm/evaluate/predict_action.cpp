85a340ae1ffc8e9df792406ea279d275ff3c84bc
David Stone <davidfromonline@gmail.com>
2023-10-27 11:39:02 -0600

    Remove unused imports

--- source/tm/evaluate/predict_action.cpp
+++ source/tm/evaluate/predict_action.cpp
@@ -11,9 +11,7 @@ export module tm.evaluate.predict_action;
 
 import tm.evaluate.depth;
 import tm.evaluate.evaluate;
-import tm.evaluate.expectiminimax;
 import tm.evaluate.move_probability;
-import tm.evaluate.scored_move;
 
 import tm.move.legal_selections;
 
@@ -21,18 +19,13 @@ import tm.environment;
 import tm.generation;
 import tm.team;
 
-import bounded;
-import containers;
-import std_module;
-
 namespace technicalmachine {
-using namespace bounded::literal;
 
 export template<Generation generation>
-auto predict_action(Team<generation> const & team, LegalSelections const team_selections, Team<generation> const & other, LegalSelections const other_selections, Environment const environment, Evaluate<generation> const evaluate, Depth const depth) -> MoveProbabilities;
+auto predict_action(Team<generation> const & team, LegalSelections const selections, Team<generation> const & other, LegalSelections const other_selections, Environment const environment, Evaluate<generation> evaluate, Depth const depth) -> MoveProbabilities;
 
 #define EXTERN_INSTANTIATION(generation) \
-	extern template auto predict_action(Team<generation> const & team, LegalSelections const team_selections, Team<generation> const & other, LegalSelections const other_selections, Environment const environment, Evaluate<generation> const evaluate, Depth const depth) -> MoveProbabilities
+	extern template auto predict_action(Team<generation> const & team, LegalSelections const selections, Team<generation> const & other, LegalSelections const other_selections, Environment const environment, Evaluate<generation> const evaluate, Depth const depth) -> MoveProbabilities
 
 TM_FOR_EACH_GENERATION(EXTERN_INSTANTIATION);
 
