d8cfee0f822bca9881b5dd90b30e987326515ee5
David Stone <davidfromonline@gmail.com>
2023-11-29 18:18:10 -0700

    Refactor `get_legal_selections` to operate on categories of actions (switching, passing, struggling, and regular moves) at a higher level. This makes it much more obvious to see which groupings are possible under what circumstances. This also fixes a few bugs around various edge cases.

--- source/tm/get_legal_selections.cpp
+++ source/tm/get_legal_selections.cpp
@@ -5,7 +5,7 @@
 
 module;
 
-#include <bounded/assert.hpp>
+#include <operators/forward.hpp>
 
 export module tm.get_legal_selections;
 
@@ -16,8 +16,8 @@ import tm.move.move_name;
 
 import tm.pokemon.active_pokemon;
 import tm.pokemon.any_pokemon;
+import tm.pokemon.max_pokemon_per_team;
 import tm.pokemon.pokemon_collection;
-import tm.pokemon.potentially_selectable_moves;
 
 import tm.ability_blocks_switching;
 import tm.any_team;
@@ -26,91 +26,135 @@ import tm.environment;
 import tm.item;
 import tm.generation;
 import tm.switch_decision_required;
-import tm.team;
 
+import bounded;
 import containers;
+import std_module;
 
 namespace technicalmachine {
+using namespace bounded::literal;
 
-template<typename PokemonType>
-constexpr auto would_switch_to_same_pokemon(PokemonCollection<PokemonType> const & collection, MoveName const move) {
-	return to_replacement(move) == collection.index();
+template<typename Function>
+struct not_fn {
+	explicit constexpr not_fn(Function function):
+		m_function(std::move(function))
+	{
+	}
+	constexpr auto operator()(auto && ... args) const {
+		return !m_function(OPERATORS_FORWARD(args)...);
+	}
+private:
+	[[no_unique_address]] Function m_function;
+};
+
+// If switching at all is legal, these are the legal switches
+using PotentialSwitches = containers::static_vector<MoveName, max_pokemon_per_team - 1_bi>;
+constexpr auto potential_switches(TeamSize const team_size, TeamIndex const active) {
+	return team_size > 1_bi ?
+		PotentialSwitches(containers::transform(
+			containers::filter(
+				containers::integer_range(team_size),
+				not_fn(bounded::equal_to(active))
+			),
+			to_switch
+		)) :
+		PotentialSwitches();
+}
+
+constexpr auto must_repeat(any_active_pokemon auto const user, Environment const environment) {
+	return user.is_encored() or is_choice_item(user.item(environment));
+}
+
+constexpr auto must_repeat_different_move(any_active_pokemon auto const user, MoveName const move, Environment const environment) {
+	auto const last_move = user.last_used_move().name();
+	return
+		!is_switch(last_move) and
+		last_move != move and
+		must_repeat(user, environment);
+}
+
+constexpr auto is_blocked_by_torment(any_active_pokemon auto const user, MoveName const move) {
+	return user.is_tormented() and user.last_used_move().name() == move;
+}
+
+constexpr auto is_legal_regular_move(
+	any_active_pokemon auto const user_pokemon,
+	any_active_pokemon auto const other_pokemon,
+	Environment const environment
+) {
+	return [=](Move const move) -> bool {
+		return
+			!must_repeat_different_move(user_pokemon, move.name(), environment) and
+			!blocks_selection_and_execution_attempt(user_pokemon, move, other_pokemon) and
+			!blocks_selection_and_execution(user_pokemon, move.name(), environment) and
+			!is_blocked_by_torment(user_pokemon, move.name());
+	};
+}
+
+constexpr auto potential_regular(
+	any_active_pokemon auto const user_pokemon,
+	any_active_pokemon auto const other_pokemon,
+	Environment const environment
+) {
+	return containers::make_static_vector(containers::transform(
+		containers::filter(
+			user_pokemon.regular_moves(),
+			is_legal_regular_move(user_pokemon, other_pokemon, environment)
+		),
+		&Move::name
+	));
 }
 
 template<any_active_pokemon ActivePokemonType>
-constexpr auto is_blocked_from_switching(ActivePokemonType const user, ActivePokemonType const other, Environment const environment) {
-	auto const block_attempted = ability_blocks_switching(other.ability(), user, environment) or user.trapped();
-	return block_attempted and !allows_switching(user.item(environment));
+constexpr auto can_switch(ActivePokemonType const user, ActivePokemonType const other, Environment const environment) {
+	auto const block_attempted =
+		ability_blocks_switching(other.ability(), user, environment) or
+		user.trapped();
+	return !block_attempted or allows_switching(user.item(environment));
 }
 
-template<Generation generation>
-constexpr auto is_illegal_switch(Team<generation> const & user, MoveName const move, ActivePokemon<generation> const other, Environment const environment) {
-	return is_switch(move) and (
-		would_switch_to_same_pokemon(user.all_pokemon(), move) or
-		is_blocked_from_switching(user.pokemon(), other, environment)
-	);
-}
-
-constexpr auto blocked_by_torment(any_active_pokemon auto const user, MoveName const move) {
-	return user.is_tormented() and user.last_used_move().name() == move and not is_switch(move) and move != MoveName::Struggle;
-}
-
-constexpr auto is_locked_in(any_active_pokemon auto const user, Environment const environment) {
-	return user.is_encored() or user.last_used_move().is_locked_in_by_move() or is_choice_item(user.item(environment));
-}
-
-constexpr auto is_locked_in_to_different_move(any_active_pokemon auto const user, MoveName const move, Environment const environment) {
-	if (not is_locked_in(user, environment)) {
-		return false;
-	}
-	auto const last_move = user.last_used_move().name();
-	return not is_switch(last_move) and last_move != move;
-}
-
-constexpr auto is_blocked_due_to_lock_in(any_active_pokemon auto const user, MoveName const move, Environment const environment) {
-	return !is_regular(move) ?
-		user.last_used_move().is_locked_in_by_move() :
-		is_locked_in_to_different_move(user, move, environment);
-}
-
-template<any_team TeamType>
-constexpr auto is_legal_selection(TeamType const & user, Move const move, TeamType const & other, Environment const environment, bool const found_selectable_move) {
-	BOUNDED_ASSERT(move != MoveName::Hit_Self);
+export template<any_real_team TeamType>
+constexpr auto get_legal_selections(
+	TeamType const & user,
+	TeamType const & other,
+	Environment const environment
+) -> LegalSelections {
+	auto replacement_switches = [&] {
+		return potential_switches(
+			user.size(),
+			user.all_pokemon().index()
+		);
+	};
 	if (switch_decision_required(user)) {
-		return is_switch(move.name()) and !would_switch_to_same_pokemon(user.all_pokemon(), move.name());
+		return LegalSelections(replacement_switches());
 	}
-	auto const is_pass = move == MoveName::Pass;
 	if (switch_decision_required(other)) {
-		return is_pass;
+		return LegalSelections({MoveName::Pass});
 	}
 	auto const user_pokemon = user.pokemon();
-	if (user_pokemon.last_used_move().moved_this_turn()) {
-		return is_pass;
+	auto const last_used_move = user_pokemon.last_used_move();
+	if (last_used_move.moved_this_turn()) {
+		return LegalSelections({MoveName::Pass});
 	}
-	if (move == MoveName::Struggle) {
-		return !found_selectable_move;
+	if (last_used_move.is_locked_in_by_move()) {
+		return LegalSelections({last_used_move.name()});
 	}
 	auto const other_pokemon = other.pokemon();
-	return
-		!is_pass and
-		!is_blocked_due_to_lock_in(user_pokemon, move.name(), environment) and
-		!is_illegal_switch(user, move.name(), other_pokemon, environment) and
-		!blocks_selection_and_execution_attempt(user_pokemon, move, other_pokemon) and
-		!blocks_selection_and_execution(user_pokemon, move.name(), environment) and
-		!blocked_by_torment(user_pokemon, move.name());
-}
-
-export template<any_team TeamType>
-constexpr auto get_legal_selections(TeamType const & user, TeamType const & other, Environment const environment) -> LegalSelections {
-	auto result = LegalSelections();
-	for (auto const move : potentially_selectable_moves(user)) {
-		bool const found_selectable_move = !containers::is_empty(result);
-		if (is_legal_selection(user, move, other, environment, found_selectable_move)) {
-			containers::push_back_into_capacity(result, move.name());
-		}
+	auto const regular = potential_regular(
+		user_pokemon,
+		other_pokemon,
+		environment
+	);
+	auto const switches = can_switch(user_pokemon, other_pokemon, environment) ?
+		replacement_switches() :
+		PotentialSwitches();
+	if (containers::is_empty(regular)) {
+		return containers::concatenate<LegalSelections>(
+			containers::array({MoveName::Struggle}),
+			switches
+		);
 	}
-	BOUNDED_ASSERT(!containers::is_empty(result));
-	return result;
+	return containers::concatenate<LegalSelections>(regular, switches);
 }
 
 } // namespace technicalmachine
