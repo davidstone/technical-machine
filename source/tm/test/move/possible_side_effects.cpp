5b1728d4eda81625e522c4335dc204babf6370fe
David Stone <davidfromonline@gmail.com>
2023-12-06 13:54:50 -0700

    Add tests for `possible_side_effects`

---
+++ source/tm/test/move/possible_side_effects.cpp
@@ -0,0 +1,57 @@
+// Copyright David Stone 2022.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+module;
+
+#include <std_module/prelude.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+export module tm.test.move.possible_side_effects;
+
+import tm.move.move_name;
+import tm.move.side_effects;
+
+import tm.pokemon.species;
+
+import tm.test.pokemon_init;
+
+import tm.environment;
+import tm.generation;
+
+import bounded;
+import containers;
+
+namespace technicalmachine {
+namespace {
+using namespace bounded::literal;
+
+TEST_CASE("Recover", "[possible_side_effects]") {
+	constexpr auto generation = Generation::four;
+	auto const environment = Environment();
+	auto const user = make_team<generation>({
+		{
+			.species = Species::Starmie,
+			.moves = {{
+				MoveName::Recover,
+			}}
+		},
+	});
+	auto const other = make_team<generation>({
+		{
+			.species = Species::Bulbasaur,
+		},
+	});
+
+	auto const recover_side_effects = possible_side_effects(
+		MoveName::Recover,
+		user.pokemon(),
+		other,
+		environment
+	);
+	CHECK(containers::size(recover_side_effects) == 1_bi);
+}
+
+} // namespace
+} // namespace technicalmachine
