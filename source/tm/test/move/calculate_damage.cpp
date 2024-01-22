9dbe8425de44962c0cc3bf3686b1f41197be4a88
David Stone <davidfromonline@gmail.com>
2024-01-22 13:31:24 -0700

    Remove unused import

--- source/tm/test/move/calculate_damage.cpp
+++ source/tm/test/move/calculate_damage.cpp
@@ -11,7 +11,6 @@ import tm.move.future_action;
 import tm.move.move;
 import tm.move.move_name;
 import tm.move.no_effect_function;
-import tm.move.other_action;
 import tm.move.power;
 
 import tm.pokemon.level;
