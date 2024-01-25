02ac9570b864d8f64a2554cacecb03855d117e25
David Stone <davidfromonline@gmail.com>
2024-01-25 12:07:47 -0700

    `Action` is now implemented with a custom variant type that allows `Action` to be the same size as `MoveName`.

--- source/tm/move/action.cpp
+++ source/tm/move/action.cpp
@@ -8,8 +8,12 @@ export module tm.move.action;
 import tm.move.move_name;
 import tm.move.switch_;
 
+import bounded;
+import numeric_traits;
 import std_module;
 import tv;
+
+using namespace bounded::literal;
 
 namespace technicalmachine {
 
@@ -18,7 +22,56 @@ export struct UnusedSwitch {
 	friend constexpr auto operator==(UnusedSwitch, UnusedSwitch) -> bool = default;
 };
 
-export using Action = tv::variant<MoveName, UnusedSwitch>;
+export struct Action {
+	constexpr Action(MoveName const move):
+		m_value(move)
+	{
+	}
+	[[noreturn]] Action(UnusedSwitch) {
+		std::unreachable();
+	}
+
+	constexpr auto index() const -> tv::variant_index<MoveName, UnusedSwitch> {
+		return tv::variant_index<MoveName, UnusedSwitch>(0_bi);
+	}
+
+	constexpr auto operator[](bounded::constant_t<0>) const -> MoveName {
+		return MoveName(m_value);
+	}
+	constexpr auto operator[](bounded::type_t<MoveName>) const -> MoveName {
+		return (*this)[0_bi];
+	}
+	auto operator[](bounded::constant_t<1>) const -> UnusedSwitch {
+		std::unreachable();
+	}
+	constexpr auto operator[](bounded::type_t<UnusedSwitch>) const -> UnusedSwitch {
+		return (*this)[1_bi];
+	}
+
+	friend constexpr auto operator==(Action, Action) -> bool = default;
+
+private:
+	using Integer = bounded::integer<
+		0,
+		bounded::normalize<numeric_traits::max_value<MoveName>>
+	>;
+	[[no_unique_address]] Integer m_value;
+
+	constexpr explicit Action(bounded::tombstone_tag, auto const make):
+		m_value(make())
+	{
+	}
+	friend bounded::tombstone_traits<Action>;
+	friend bounded::tombstone_traits_composer<&Action::m_value>;
+};
+
+} // namespace technicalmachine
+
+template<>
+struct bounded::tombstone_traits<technicalmachine::Action> : bounded::tombstone_traits_composer<&technicalmachine::Action::m_value> {
+};
+
+namespace technicalmachine {
 
 export constexpr auto get_move(Action const action) -> MoveName {
 	return tv::visit(action, tv::overload(
