39ba73d7f980ccb5f3d7eb585a840bc58224e1f2
David Stone <davidfromonline@gmail.com>
2024-10-04 13:22:54 -0600

    `score_predict_action` now supports `net_hp` and `expectimax`

--- source/tm/test/ps_usage_stats/score_predict_action.cpp
+++ source/tm/test/ps_usage_stats/score_predict_action.cpp
@@ -16,6 +16,9 @@ import tm.clients.ps.slot_memory;
 
 import tm.clients.party;
 
+import tm.evaluate.all_evaluate;
+import tm.evaluate.depth;
+
 import tm.move.move_name;
 import tm.move.pass;
 import tm.move.selection;
@@ -33,6 +36,7 @@ import tm.ps_usage_stats.worker;
 
 import tm.strategy.expectimax;
 import tm.strategy.max_damage;
+import tm.strategy.net_hp;
 import tm.strategy.random_selection;
 import tm.strategy.statistical;
 import tm.strategy.strategy;
@@ -69,6 +73,50 @@ struct ParsedArgs {
 	Strategy strategy;
 };
 
+auto parse_strategy(int argc, char const * const * argv) -> Strategy {
+	auto const name = std::string_view(argv[0]);
+	if (name == "random"sv) {
+		if (argc != 1) {
+			throw std::runtime_error("random strategy accepts no arguments");
+		}
+		return make_random_selection();
+	} else if (name == "random_weighted"sv) {
+		if (argc != 2) {
+			throw std::runtime_error("random_weighted strategy requires probability argument");
+		}
+		return make_random_selection(std::stod(argv[1]));
+	} else if (name == "max_damage"sv) {
+		if (argc != 1) {
+			throw std::runtime_error("max_damage strategy accepts no arguments");
+		}
+		return make_max_damage();
+	} else if (name == "net_hp"sv) {
+		if (argc != 1) {
+			throw std::runtime_error("net_hp strategy accepts no arguments");
+		}
+		return make_net_hp();
+	} else if (name == "statistical"sv) {
+		if (argc != 1) {
+			throw std::runtime_error("statistical strategy accepts no arguments");
+		}
+		return make_statistical();
+	} else if (name == "expectimax"sv) {
+		if (argc < 4) {
+			throw std::runtime_error("expectimax strategy requires two depth arguments followed by a foe strategy");
+		}
+		return make_expectimax(
+			AllEvaluate(),
+			Depth(
+				bounded::to_integer<DepthInt>(argv[1]),
+				bounded::to_integer<DepthInt>(argv[2])
+			),
+			parse_strategy(argc - 3, argv + 3)
+		);
+	} else {
+		throw std::runtime_error("Selection strategy must be one of random, random_weighted, max_damage, net_hp, statistical");
+	}
+}
+
 auto parse_args(int argc, char const * const * argv) -> ParsedArgs {
 	if (argc < 4) {
 		throw std::runtime_error("Usage is score_predict_selection thread_count input_directory strategy");
@@ -78,36 +126,10 @@ auto parse_args(int argc, char const * const * argv) -> ParsedArgs {
 	if (!std::filesystem::exists(input_directory)) {
 		throw std::runtime_error(containers::concatenate<std::string>(input_directory.string(), " does not exist"sv));
 	}
-	auto strategy = [=] -> Strategy {
-		auto const name = std::string_view(argv[3]);
-		if (name == "random"sv) {
-			if (argc != 4) {
-				throw std::runtime_error("random strategy accepts no arguments");
-			}
-			return make_random_selection();
-		} else if (name == "random_weighted"sv) {
-			if (argc != 5) {
-				throw std::runtime_error("random_weighted strategy requires probability argument");
-			}
-			return make_random_selection(std::stod(argv[4]));
-		} else if (name == "max_damage"sv) {
-			if (argc != 4) {
-				throw std::runtime_error("max_damage strategy accepts no arguments");
-			}
-			return make_max_damage();
-		} else if (name == "statistical"sv) {
-			if (argc != 4) {
-				throw std::runtime_error("statistical strategy accepts no arguments");
-			}
-			return make_statistical();
-		} else {
-			throw std::runtime_error("Selection strategy must be one of random, random_weighted, max_damage, statistical");
-		}
-	}();
 	return ParsedArgs{
 		thread_count,
 		std::move(input_directory),
-		std::move(strategy)
+		parse_strategy(argc - 3, argv + 3)
 	};
 }
 
