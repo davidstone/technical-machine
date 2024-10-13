c0aaa670997cc75d82a78f81684254027186af79
David Stone <davidfromonline@gmail.com>
2024-10-12 23:36:03 -0600

    `UsageStatsProbabilities` member functions can all be `constexpr`

--- source/tm/team_predictor/usage_stats_probabilities.cpp
+++ source/tm/team_predictor/usage_stats_probabilities.cpp
@@ -28,28 +28,27 @@ export struct UsageStatsProbabilities {
 		using key_type = T;
 		using mapped_type = float;
 		using Map = containers::flat_map<key_type, mapped_type>;
-		Data() {
-		}
+		Data() = default;
 
-		explicit Data(bounded::explicitly_convertible_to<Map> auto && source):
+		constexpr explicit Data(bounded::explicitly_convertible_to<Map> auto && source):
 			m_map(OPERATORS_FORWARD(source))
 		{
 		}
 
-		auto map() const -> Map const & {
+		constexpr auto map() const -> Map const & {
 			return m_map;
 		}
 
-		auto operator()(key_type const move) const -> mapped_type {
+		constexpr auto operator()(key_type const move) const -> mapped_type {
 			auto const element = containers::lookup(m_map, move);
 			return element ? *element : 0.0F;
 		}
 
-		auto insert(auto && range) & {
+		constexpr auto insert(auto && range) & {
 			m_map.insert(OPERATORS_FORWARD(range));
 		}
 		template<typename K = key_type>
-		auto lazy_insert(K && key, bounded::construct_function_for<mapped_type> auto && mapped) {
+		constexpr auto lazy_insert(K && key, bounded::construct_function_for<mapped_type> auto && mapped) {
 			return m_map.lazy_insert(OPERATORS_FORWARD(key), OPERATORS_FORWARD(mapped));
 		}
 	private:
@@ -68,11 +67,11 @@ export struct UsageStatsProbabilities {
 	{
 	}
 
-	auto operator()(Species const species) const -> Inner const * {
+	constexpr auto operator()(Species const species) const -> Inner const * {
 		return containers::lookup(m_map, species);
 	}
 
-	auto map() const -> Map const & {
+	constexpr auto map() const -> Map const & {
 		return m_map;
 	}
 
