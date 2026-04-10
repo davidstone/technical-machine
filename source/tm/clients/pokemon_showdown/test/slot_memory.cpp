b9c0099194fd5eb397c65578dfd3d071c97c8750
David Stone <davidfromonline@gmail.com>
2026-04-10 13:56:43 -0600

    `source/tm/CMakeLists.txt` no longer knows about Pokemon Showdown-specific things

--- source/tm/clients/pokemon_showdown/test/slot_memory.cpp
+++ source/tm/clients/pokemon_showdown/test/slot_memory.cpp
@@ -3,7 +3,11 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
+module;
+
 #include <catch2/catch_test_macros.hpp>
+
+export module tm.test.clients.ps.slot_memory;
 
 import tm.clients.ps.slot_memory;
 
