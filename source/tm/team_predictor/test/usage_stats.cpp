76c78c249887f1c0bc8e04c2731d32361163f403
David Stone <davidfromonline@gmail.com>
2026-04-10 14:48:08 -0600

    `source/tm/CMakeLists.txt` no longer knows about team predictor-specific things

--- source/tm/team_predictor/test/usage_stats.cpp
+++ source/tm/team_predictor/test/usage_stats.cpp
@@ -3,7 +3,11 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
+module;
+
 #include <catch2/catch_test_macros.hpp>
+
+export module tm.test.team_predictor.usage_stats;
 
 import tm.move.move_name;
 
