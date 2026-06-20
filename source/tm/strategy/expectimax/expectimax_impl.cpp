d31bda77a087da5cf819833c6e7ad11b8f1b57a0
David Stone <davidfromonline@gmail.com>
2026-06-20 12:02:19 -0600

    Move `score_executed_actions` to its own module

--- source/tm/strategy/expectimax/expectimax_impl.cpp
+++ source/tm/strategy/expectimax/expectimax_impl.cpp
@@ -21,8 +21,6 @@ import tm.evaluate.transposition;
 import tm.evaluate.victory;
 import tm.evaluate.win;
 
-import tm.move.actual_damage;
-import tm.move.call_move;
 import tm.move.category;
 import tm.move.future_selection;
 import tm.move.irrelevant_action;
@@ -32,9 +30,7 @@ import tm.move.move_name;
 import tm.move.other_action;
 import tm.move.pass;
 import tm.move.selection;
-import tm.move.side_effects;
 import tm.move.switch_;
-import tm.move.used_move;
 
 import tm.pokemon.active_pokemon;
 import tm.pokemon.any_pokemon;
@@ -44,7 +40,6 @@ import tm.pokemon.pokemon;
 import tm.pokemon.pokemon_collection;
 import tm.pokemon.species;
 
-import tm.stat.chance_to_hit;
 import tm.stat.faster;
 
 import tm.status.clears_status;
@@ -53,6 +48,7 @@ import tm.status.status_name;
 import tm.strategy.expectimax.execute_switch;
 import tm.strategy.expectimax.generic_flag_branch;
 import tm.strategy.expectimax.moved;
+import tm.strategy.expectimax.score_executed_actions;
 import tm.strategy.expectimax.score_selections;
 import tm.strategy.expectimax.to_selection_probabilities;
 
@@ -65,8 +61,6 @@ import tm.type.move_type;
 
 import tm.ability;
 import tm.any_team;
-import tm.contact_ability_effect;
-import tm.critical_hit_probability;
 import tm.end_of_turn;
 import tm.end_of_turn_flags;
 import tm.environment;
@@ -125,89 +119,6 @@ constexpr auto fainting_forces_end_of_turn(
 		default:
 			return is_fainted(selected.selection);
 	}
-}
-
-struct SelectedAndExecuted {
-	MoveName selected;
-	MoveName executed;
-};
-
-constexpr auto paralysis_probability(StatusName const status) -> Probability {
-	return status == StatusName::paralysis ? Probability(0.25) : Probability(0.0);
-}
-
-template<Generation generation>
-auto execute_move(
-	State<generation> const & state,
-	Selector const select,
-	SelectedAndExecuted const move,
-	OtherAction const other_action,
-	Depth const depth,
-	auto const continuation
-) -> Score {
-	auto const selected = select(state);
-	auto const user_pokemon = selected.selection.pokemon();
-	auto const other_pokemon = selected.other.pokemon();
-	auto const side_effects = possible_side_effects(move.executed, user_pokemon, selected.other, state.environment);
-	auto const status = user_pokemon.status();
-	auto const probability_of_clearing_status = status.probability_of_clearing(generation, user_pokemon.ability());
-	auto const specific_chance_to_hit = chance_to_hit(
-		user_pokemon,
-		KnownMove(
-			move.executed,
-			move_type(generation, move.executed, get_hidden_power_type(user_pokemon))
-		),
-		other_pokemon,
-		state.environment,
-		moved(other_pokemon)
-	);
-	auto const ch_probability = critical_hit_probability(user_pokemon, move.executed, other_pokemon.ability(), state.environment);
-	auto const chance_to_be_paralyzed = paralysis_probability(user_pokemon.status().name());
-	auto const action_end_probability = user_pokemon.last_used_move().action_end_probability();
-	return generic_flag_branch(probability_of_clearing_status, [&](bool const clear_status) {
-		return generic_flag_branch(specific_chance_to_hit, [&](bool const hits) {
-			return generic_flag_branch(chance_to_be_paralyzed, [&](bool const is_fully_paralyzed) {
-				return generic_flag_branch(action_end_probability, [&](bool const action_ends) {
-					return containers::sum(containers::transform(
-						side_effects,
-						[&](auto const & side_effect) {
-							return side_effect.probability * generic_flag_branch(
-								hits ? ch_probability : Probability(0.0),
-								[&](bool const critical_hit) {
-									auto copy = state;
-									auto const selected_copy = select(copy);
-									// TODO: https://github.com/davidstone/technical-machine/issues/24
-									constexpr auto contact_ability_effect = ContactAbilityEffect::nothing;
-									call_move(
-										selected_copy.selection,
-										UsedMove<Team<generation>>(
-											move.selected,
-											move.executed,
-											critical_hit,
-											!hits,
-											action_ends,
-											contact_ability_effect,
-											side_effect.function
-										),
-										selected_copy.other,
-										other_action,
-										copy.environment,
-										clear_status,
-										ActualDamage::Unknown{},
-										is_fully_paralyzed
-									);
-									if (auto const won = win(copy.ai, copy.foe)) {
-										return *won + Score(double(depth.remaining_general()));
-									}
-									return continuation(copy);
-								}
-							);
-						}
-					));
-				});
-			});
-		});
-	});
 }
 
 template<Generation generation>
@@ -821,45 +732,6 @@ private:
 		return max_score(start_of_turn_action(state, depth));
 	}
 
-
-	auto score_executed_actions(
-		State<generation> const & state,
-		Selector const select,
-		Selection const selection,
-		OtherAction const other_action,
-		Depth const depth,
-		auto const continuation
-	) const -> Score {
-		return tv::visit(selection, tv::overload(
-			[&](Switch const switch_) -> Score {
-				return execute_switch(state, select, switch_, depth, continuation);
-			},
-			[&](MoveName const selected) -> Score {
-				auto const & team = select(state).selection;
-				if (team.pokemon().hp().current() == 0_bi) {
-					return continuation(state);
-				}
-				auto const executed_moves = possible_executed_moves(selected, team);
-				auto const sum = containers::sum(containers::transform(
-					executed_moves,
-					[&](MoveName const executed) {
-						return execute_move(
-							state,
-							select,
-							SelectedAndExecuted(selected, executed),
-							other_action,
-							depth,
-							continuation
-						);
-					}
-				));
-				return sum / double(containers::size(executed_moves));
-			},
-			[&](Pass) -> Score {
-				return continuation(state);
-			}
-		));
-	}
 
 	auto get_foe_selections(
 		State<generation> const & state,
